d943b1297cd1662984f508fcd9e471dd7b90f839
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-26 10:26:49 +0800

    feat: modal provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CreateRestroom from "@/components/modals/create-restroom-modal";
+
+const ModalProvider = () => {
+  return (
+    <>
+      <CreateRestroom />
+    </>
+  );
+};
+
+export default ModalProvider;
