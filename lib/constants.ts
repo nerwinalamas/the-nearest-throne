896f08aca8ca08af708faa847738266b0fe8bbb2
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-27 12:18:32 +0800

    feat: constants

---
+++ lib/constants.ts
@@ -0,0 +1,24 @@
+export const ACCESSIBILITY_FEATURES = [
+  "Wheelchair Accessible",
+  "Hand Dryer",
+  "Paper Towels",
+  "Soap Available",
+  "Tabo",
+  "Bidet",
+];
+
+export const PAYMENT_TYPES = ["Free", "Paid"];
+
+export const TOILET_TYPES = [
+  "All",
+  "Public Restrooms Only",
+  "Private/Business Restrooms Only",
+];
+
+export const GENDER_OPTIONS = [
+  "Male",
+  "Female",
+  "Gender-Neutral",
+  "All-Gender",
+  "Unisex",
+];
