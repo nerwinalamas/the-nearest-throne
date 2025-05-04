14136b04730dc407a88e6f85cbc9930169d1d0d9
Nerwin Alamas <nerwinalamas@gmail.com>
2025-05-04 11:37:20 +0800

    refactor: add view reviews drawer

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import ViewReviewsDrawer from "@/components/drawers/view-reviews-drawer";
 import CreateRestroom from "@/components/modals/create-restroom-modal";
 
 const ModalProvider = () => {
   return (
     <>
       <CreateRestroom />
+      <ViewReviewsDrawer />
     </>
   );
 };
