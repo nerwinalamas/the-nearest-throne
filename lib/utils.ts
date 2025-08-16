94be1e5fca8d229fe5ad73b08be5b0109e25fe1a
Nerwin Alamas <nerwinalamas@gmail.com>
2025-08-16 12:07:17 +0800

    feat: get user initials

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Generate initials from name
+export const getInitials = (name?: string) => {
+  if (!name) return "U";
+
+  return name
+    .split(" ")
+    .map((n) => n[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2); // Limit to 2 characters max
+};
